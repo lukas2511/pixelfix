68a30b0b26fc7ece5778453e420c553a1f52051d
Lukas Schauer <lukas@schauer.so>
2014-09-27 21:14:06 +0200

    Initial Commit

---
+++ pixelfix/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  pixelfix
+//
+//  Created by Lukas Schauer on 27/09/14.
+//  Copyright (c) 2014 Lukas Schauer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ pixelfix/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  pixelfix
+//
+//  Created by Lukas Schauer on 27/09/14.
+//  Copyright (c) 2014 Lukas Schauer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
